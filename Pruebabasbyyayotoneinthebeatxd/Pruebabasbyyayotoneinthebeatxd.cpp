e44f7dadc22e7ec563085b26dff0813269c3f7d7
maupe <maupe@DESKTOP-CKELBAU>
2025-05-29 12:31:16 -0600

    Agregar archivos de proyecto.

---
+++ Pruebabasbyyayotoneinthebeatxd/Pruebabasbyyayotoneinthebeatxd.cpp
@@ -0,0 +1,25 @@
+// Pruebabasbyyayotoneinthebeatxd.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Yayoyayotoneeeeeeinthebeaaaaatxdhaceunañoquenoprogramoconyayotoneinthebeatxd!\n";
+	printf("when haces tus momos en visual studio 2022");
+	printf("El futuro es hoy oiste viejo");
+
+	printf("but el programa no compila");
+	printf("ooooh mi lente de contacto :vvvvvv arroba angel (aunque no use lentes de contacto xdddd)");
+	printf("aaarrrrrrrrrrrrroba");
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
